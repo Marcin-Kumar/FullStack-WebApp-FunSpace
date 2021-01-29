698243029d81e47a216fa72792f93a4f02a0a94c
Marcin-Kumar <marcinkumar@gmail.com>
2021-01-29 19:04:26 +0100

    Added edit a location rest routes and functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Campground = require("./models/campground");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/fun-space", {
 	useNewUrlParser: true,
@@ -21,6 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -41,12 +43,30 @@ app.get("/locations/:id", async (req, res) => {
 	res.render("locations/show", { location });
 });
 
+app.get("/locations/:id/edit", async (req, res) => {
+	const { id } = req.params;
+	const location = await Campground.findById(id);
+	res.render("locations/edit", { location });
+});
+
 app.post("/locations", async (req, res) => {
 	const details = req.body.campground;
 	const location = new Campground(details);
 	await location.save();
 	const locations = await Campground.find({});
 	res.redirect(`/locations/${location._id}`);
+});
+
+app.put("/locations/:id", async (req, res) => {
+	const { id } = req.params;
+	const location = await Campground.findByIdAndUpdate(
+		id,
+		{ ...req.body.campground },
+		{
+			new: true
+		}
+	);
+	res.render("locations/show", { location });
 });
 
 app.listen(3000, () => {
